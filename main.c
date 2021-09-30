60f7b13bb3421f1c9981b09ccdfe401deb741bc2
Benny <testabenedetto2002@gmail.com>
2021-09-30 22:09:40 +0200

    Inizalizzazione

--- main.c
+++ main.c
@@ -4,8 +4,20 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int NumTo
+	int NumToTras;
+	int Ndigit;
+	float Digit;
+	int div = 1;
 	
+	printf("Inserisci il numero da convertire: ");
+	scanf("%d", &NumToTras);
+	
+	while(NumToTras>0)
+	{	
+		NumToTras = NumToTras/10;
+	}
+	
+	printf("%1.0f",Digit);
 	
 	return 0;
 }
