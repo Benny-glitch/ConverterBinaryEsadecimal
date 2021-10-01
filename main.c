0f8b7bc34c69f89ce3ba88678bfe87b253cb0d3f
Benny <testabenedetto2002@gmail.com>
2021-10-01 11:29:33 +0200

    aggiusta codice che scompone il numero

--- main.c
+++ main.c
@@ -6,18 +6,30 @@
 int main(int argc, char *argv[]) {
 	int NumToTras;
 	int Ndigit;
-	float Digit;
 	int div = 1;
+	int i = 0;
+	int Digitarray[100];
+	int s = 0;
+	int ciao = 10;
 	
 	printf("Inserisci il numero da convertire: ");
 	scanf("%d", &NumToTras);
 	
-	while(NumToTras>0)
-	{	
-		NumToTras = NumToTras/10;
-	}
+		for(i = 0; NumToTras > 0; i++)
+		{				
+		 	Digitarray[i] = NumToTras%10;
+			NumToTras = NumToTras/10;							
+		}	
+			
 	
-	printf("%1.0f",Digit);
+		printf("\n");
+		printf("\n%d", Digitarray[0]);	
+		printf("\n%d", Digitarray[1]);
+		printf("\n%d", Digitarray[2]);
+		printf("\n%d", Digitarray[3]);
+		printf("\n%d", Digitarray[4]);
+		printf("\n%d", Digitarray[5]);
+			
 	
 	return 0;
 }
