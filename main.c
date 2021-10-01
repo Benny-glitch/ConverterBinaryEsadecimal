549daf1509ec43e72e0b3a21a8b668b4fa23eff8
Benny <testabenedetto2002@gmail.com>
2021-10-01 14:41:49 +0200

    Quasi finito

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "math.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
@@ -8,28 +9,39 @@ int main(int argc, char *argv[]) {
 	int Ndigit;
 	int div = 1;
 	int i = 0;
-	int Digitarray[100];
+	int c = 0;
 	int s = 0;
-	int ciao = 10;
+	int rest = 0;
+	int Digitarray[s];	
 	
 	printf("Inserisci il numero da convertire: ");
 	scanf("%d", &NumToTras);
-	
+		
+		while(NumToTras != 0)
+		{
+			rest = NumToTras;
+			rest = rest%2;			
+			Digitarray[i] = rest;
+			printf("\n%d", Digitarray[i]);
+			NumToTras = NumToTras/2;				
+			i++;								
+		}
+		
+			
+		printf("\n%d", i);
+		for(c = i - 1; c >= 0; c--)	
+		{
+			printf(" %d ", Digitarray[c]);
+		}	
+			
+				
+		/*
 		for(i = 0; NumToTras > 0; i++)
 		{				
 		 	Digitarray[i] = NumToTras%10;
-			NumToTras = NumToTras/10;							
+			NumToTras = NumToTras/10;								
 		}	
+		*/
 			
-	
-		printf("\n");
-		printf("\n%d", Digitarray[0]);	
-		printf("\n%d", Digitarray[1]);
-		printf("\n%d", Digitarray[2]);
-		printf("\n%d", Digitarray[3]);
-		printf("\n%d", Digitarray[4]);
-		printf("\n%d", Digitarray[5]);
-			
-	
 	return 0;
 }
